9d03c5e9c90edd71227536ecc1d7e83afcbfc861
cottephi <cottephi@gmail.com>
2017-07-20 11:22:58 +0200

    added autodetect of already present datafile in copy_data.sh

--- copy_data.sh
+++ copy_data.sh
@@ -1,17 +1,36 @@
 #! /bin/bash
 
-scp wa105db@lxplus.cern.ch:~/tools/pcotte_getdata/Data_* ./
-for data in "./Data_*" ; do
-  if [ -d $data ] ; then
-    echo "ERROR: $data is a directory"
-    exit 1
+for Dir in ./dir_* ; do
+  dir_data=$(basename $Dir)
+  if ! [ -d "$dir_data" ] ; then
+    continue
   fi
-  direc=$(echo $data | sed -r 's/Data_//g')
-  if [ -d $direc ] ; then
-    echo "Folder $direc already exists. Skipping it."
+  if ! ls $dir_data/Data_* 1> /dev/null 2>&1; then
+    continue
+  fi
+  cp $dir_data/Data_* ./
+done
+
+rsync -avh -e ssh wa105db@lxplus.cern.ch:~/tools/pcotte_getdata/Data_* ./
+
+if ! ls Data_* 1> /dev/null 2>&1; then
+  echo "No Data file transfered"
+  exit 1
+fi
+
+for data in ./Data_* ; do
+  File=$(basename $data)
+  if [ -d "$File" ] ; then
+    echo "$File is a directory. Skipping it."
+    continue
+  fi
+  direc=$(echo $File | sed -r 's/Data_//g')
+  direc="dir_$direc"
+  if [ -d "$direc" ] ; then
+    rm $File
     continue
  fi
   mkdir $direc
-  file=$(basename $data)
+  file=$(basename $File)
   mv $file $direc/
 done
