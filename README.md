ec8eb310607337faf6547a61c3d8ee2777e47541
cottephi <cottephi@gmail.com>
2017-07-20 12:33:08 +0200

    plot_data.py now ignores param not found in param_list or datafile and does not try to plot the corresponding graphs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # WA105_plots
 Scripts to copy data and plot histograms from wa105db
+
+-Get all the files named Data_* from wa105db/tools/pcotte_getData that you do not already have, using copy_data.sh (no argument)
+-Plot with plot_data.sh, need at least the file to analyse as an argument (folder is not enough, need the file). Can also specify the start and end times (4 arguments since need a space between day and hour)
+-Python scripts in pyfiles : 
+  -plot_functions.py contains all the different functions that could plot what you want. Feel free to add those you might need and do not already exist.
+  -plot_data.py is where you will call those functions.
+-param_list.txt contains all the parameters that should be in the datafile. If a parameter asked in plot_data.py is not in the param_list.txt, it is ignored. Same if it is in param_list.txt but not in datafile. 
+
+WARNING: Do not remove any function in plot_function.py, just comment them in plot_data.py. Same pour parameters in param_list.
+
--- pyfiles/plot_data.py
+++ pyfiles/plot_data.py
@@ -21,15 +21,15 @@ def primer_load_data(wanted_param):
   command = "sed -n -e '/^" + wanted_param + "/p' ./param_list.txt | cut -d ' ' -f 2"
   code = subprocess.check_output([command], shell=True)
   if len(code) == 0:
-    print("ERROR: " + wanted_param + " not found in ./param_list.txt")
-    exit(0)
-    
+    print("WARNING: " + wanted_param + " not found in ./param_list.txt. Ignored.")
+    return ""
+
   code = code[:-1]
   first_line = subprocess.check_output("head -n1 " + sys.argv[3], shell=True)
   if code not in first_line:
-    print("ERROR: " + code + ", code for " + wanted_param + ", not found in data file")
-    exit(0)
-    
+    print("WARNING: " + code + ", code for " + wanted_param + ", not found in data file")
+    return ""
+
   code_list = first_line.split(' ')
   pos = code_list.index(code)
   pos = pos + 1
@@ -44,18 +44,21 @@ def load_data(wanted_param):
   time = numpy.array( numpy.reshape(time, len(time)) )
   if wanted_param != "time":
     param = primer_load_data(wanted_param)
+    if param == "":
+      return "ignore"
     param = numpy.array( numpy.reshape(param, len(time)) )
     return param.astype(numpy.float)
   else:
     return time.astype(numpy.float)
-    
-    
-    
-    
+
+
+
+
+
 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 print("Never use parameters name such as 'temp_1', 'temp_2' ... 'temp_10'... otherwise the 'sed' command of 'primer_load_data' will select both 'temp_1' and 'temp_10' when looking for 'temp_1', resulting in a crash. It is recommanded to put such numbering at the beginning of the name.")
 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!END OF WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    
+
 ###################################################################
 ###################################################################
 ###################################################################
@@ -66,13 +69,16 @@ print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!END OF WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 ##################################### time
 time = load_data("time")
+if time[0] == "ignore":
+  print("ERROR loading time. Plot can not continue.")
+  exit(1)
 
 ##################################### Temperatures
 temperatures = []
 for i in range(0,12):
   temp = ( "%i_TE_RbCh2" %(i+1) )
   temperatures.append(load_data(temp))
-  
+
 Hall_T = load_data("Hall_T")
 
 ##################################### Pressures
@@ -129,6 +135,7 @@ Cathode_I = load_data("Cathode_I")
 ##################################### Cathode V
 Cathode_V = load_data("Cathode_V")
 
+
 ###################### Boxes
 #Box_1_1 = [Cathode_V, Cathode_I, GRID_1_V, GRID_1_I, levelmeters[0], LEM_Up_I[0], LEM_Down_I[0], LEM_Up_V[0], LEM_Down_V[0]]
 #Box_1_3 = [Cathode_V, Cathode_I, GRID_1_V, GRID_1_I, levelmeters[0], LEM_Up_I[2], LEM_Down_I[2], LEM_Up_V[2], LEM_Down_V[2]]
@@ -163,91 +170,122 @@ else:
 
 
 ###################### Pressures
-if not os.path.isdir(sys.argv[4] + "/pressures"):
-  os.system("mkdir " + sys.argv[4] + "/pressures")
- 
-a = plot_pressures(time, date, pressures[0], "Abs_Pressure_in_Tank", "./" + sys.argv[4] + "/pressures/", tmin, tmax)
+if pressures[0][0] != "ignore":
+  if not os.path.isdir(sys.argv[4] + "/pressures"):
+    os.system("mkdir " + sys.argv[4] + "/pressures")
+  a = plot_pressures(time, date, pressures[0], "Abs_Pressure_in_Tank", "./" + sys.argv[4] + "/pressures/", tmin, tmax)
+if pressures[1][0] != "ignore":
+  if not os.path.isdir(sys.argv[4] + "/pressures"):
+    os.system("mkdir " + sys.argv[4] + "/pressures")
 a = plot_pressures(time, date, pressures[1], "Diff_Pressure_Tank-Insulation", "./" + sys.argv[4] + "/pressures/", tmin, tmax)
+if pressures[2][0] != "ignore":
+  if not os.path.isdir(sys.argv[4] + "/pressures"):
+    os.system("mkdir " + sys.argv[4] + "/pressures")
 a = plot_pressures(time, date, pressures[2], "Diff_Pressure_Tank_Atm", "./" + sys.argv[4] + "/pressures/", tmin, tmax)
 
 ###################### ribon chain 2 temperatures
-if not os.path.isdir(sys.argv[4] + "/temperatures"):
-  os.system("mkdir " + sys.argv[4] + "/temperatures")
- 
-a = plot_temperatures(time, date, temperatures, "Temp_Ribon_Chain_2", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
-a = plot_temperatures(time, date, [temperatures[3],temperatures[4]], "Temp_Ribon_Chain_2_4-5", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
-a = plot_temperatures(time, date, temperatures[3], "Temp_Ribon_Chain_2_4", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
-a = plot_temperatures(time, date, temperatures[4], "Temp_Ribon_Chain_2_5", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+plot_temps = True
+for i in range(0,len(temperatures)):
+  if temperatures[i][0] == "ignore":
+    plot_temps = False
+if plot_temps == True:
+  if not os.path.isdir(sys.argv[4] + "/temperatures"):
+    os.system("mkdir " + sys.argv[4] + "/temperatures")
+  a = plot_temperatures(time, date, temperatures, "Temp_Ribon_Chain_2", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+  a = plot_temperatures(time, date, [temperatures[3],temperatures[4]], "Temp_Ribon_Chain_2_4-5", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+  a = plot_temperatures(time, date, temperatures[3], "Temp_Ribon_Chain_2_4", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+  a = plot_temperatures(time, date, temperatures[4], "Temp_Ribon_Chain_2_5", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+else:
+  if temperatures[3][0] != "ignore" and temperatures[4][0] != "ignore":
+    if not os.path.isdir(sys.argv[4] + "/temperatures"):
+      os.system("mkdir " + sys.argv[4] + "/temperatures")
+    a = plot_temperatures(time, date, [temperatures[3],temperatures[4]], "Temp_Ribon_Chain_2_4-5", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+    a = plot_temperatures(time, date, temperatures[3], "Temp_Ribon_Chain_2_4", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+    a = plot_temperatures(time, date, temperatures[4], "Temp_Ribon_Chain_2_5", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+  else:
+    if temperatures[3][0] != "ignore":
+      if not os.path.isdir(sys.argv[4] + "/temperatures"):
+        os.system("mkdir " + sys.argv[4] + "/temperatures")
+      a = plot_temperatures(time, date, temperatures[3], "Temp_Ribon_Chain_2_4", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
+    
+    if temperatures[4][0] != "ignore":
+      if not os.path.isdir(sys.argv[4] + "/temperatures"):
+        os.system("mkdir " + sys.argv[4] + "/temperatures")
+      a = plot_temperatures(time, date, temperatures[4], "Temp_Ribon_Chain_2_4", "./" + sys.argv[4] + "/temperatures/", tmin, tmax)
 
 ###################### Interpolated + fft
-if not os.path.isdir(sys.argv[4] + "/interpolated"):
-  os.system("mkdir " + sys.argv[4] + "/interpolated")
-  
-var = []
-title = []
-ytitle = []
-ytitlefft = []
-var.append(Cathode_V)
-var.append(Cathode_I)
-title.append("CathodeV")
-title.append("Cathode_I")
-ytitle.append("Voltage [kV]")
-ytitle.append("Current [uA]")
-ytitlefft.append("kV.s")
-ytitlefft.append("uA.s")
-a = Interpolate(time, date, var, title, ytitle, ytitlefft, "./" + sys.argv[4] + "/interpolated/", tmin, tmax)  
+if Cathode_V[0] != "ignore" and Cathode_I[0] != "ignore":
+  if not os.path.isdir(sys.argv[4] + "/interpolated"):
+    os.system("mkdir " + sys.argv[4] + "/interpolated")
+  var = []
+  title = []
+  ytitle = []
+  ytitlefft = []
+  var.append(Cathode_V)
+  var.append(Cathode_I)
+  title.append("CathodeV")
+  title.append("Cathode_I")
+  ytitle.append("Voltage [kV]")
+  ytitle.append("Current [uA]")
+  ytitlefft.append("kV.s")
+  ytitlefft.append("uA.s")
+  a = Interpolate(time, date, var, title, ytitle, ytitlefft, "./" + sys.argv[4] + "/interpolated/", tmin, tmax)  
 
 ###################### LEMs + grid
-if not os.path.isdir(sys.argv[4] + "/LEM_GRID"):
-  os.system("mkdir " + sys.argv[4] + "/LEM_GRID")
-  
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[0], LEM_Down_I[0], LEM_Up_V[0], LEM_Up_I[0], tmin, tmax, "LEM_1")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[1], LEM_Down_I[1], LEM_Up_V[1], LEM_Up_I[1], tmin, tmax, "LEM_2")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[2], LEM_Down_I[2], LEM_Up_V[2], LEM_Up_I[2], tmin, tmax, "LEM_3")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[3], LEM_Down_I[3], LEM_Up_V[3], LEM_Up_I[3], tmin, tmax, "LEM_4")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[4], LEM_Down_I[4], LEM_Up_V[4], LEM_Up_I[4], tmin, tmax, "LEM_5")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[5], LEM_Down_I[5], LEM_Up_V[5], LEM_Up_I[5], tmin, tmax, "LEM_6")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[6], LEM_Down_I[6], LEM_Up_V[6], LEM_Up_I[6], tmin, tmax, "LEM_7")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[7], LEM_Down_I[7], LEM_Up_V[7], LEM_Up_I[7], tmin, tmax, "LEM_8")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[8], LEM_Down_I[8], LEM_Up_V[8], LEM_Up_I[8], tmin, tmax, "LEM_9")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[9], LEM_Down_I[9], LEM_Up_V[9], LEM_Up_I[9], tmin, tmax, "LEM_10")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[10], LEM_Down_I[10], LEM_Up_V[10], LEM_Up_I[10], tmin, tmax, "LEM_11")
-a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[11], LEM_Down_I[11], LEM_Up_V[11], LEM_Up_I[11], tmin, tmax, "LEM_12")
+if GRID_1_V[0] != "ignore" and GRID_1_I[0] != "ignore":
+  for i in range(0,len(LEM_Down_V)):
+    if LEM_Down_V[i][0] != "ignore" and LEM_Down_I[i][0] != "ignore" and LEM_Up_V[i][0] != "ignore" and LEM_Up_I[i][0] != "ignore":
+      if not os.path.isdir(sys.argv[4] + "/LEM_GRID"):
+        os.system("mkdir " + sys.argv[4] + "/LEM_GRID")
+      title = "LEM_%i" %(i+1)
+      a = plot_LEM_GRID(time, date, GRID_1_V, GRID_1_I, LEM_Down_V[i], LEM_Down_I[i], LEM_Up_V[i], LEM_Up_I[i], tmin, tmax, title)
 
-if not os.path.isdir(sys.argv[4] + "/LEMs"):
-  os.system("mkdir " + sys.argv[4] + "/LEMs")
-
+###################### LEMs alone
 for i in range(0,12):
-  title = "LEM_%i " %(i+1)
-  a = plot_single_LEM(time, date, LEM_Up_I[i], LEM_Down_I[i], Hall_T, title, tmin, tmax)
+  if LEM_Up_I[i][0] != "ignore" and LEM_Down_I[i][0] != "ignore" and Hall_T[0] != "ignore":
+    title = "LEM_%i_" %(i+1)
+    if not os.path.isdir(sys.argv[4] + "/LEMs"):
+      os.system("mkdir " + sys.argv[4] + "/LEMs")
+    a = plot_single_LEM(time, date, LEM_Up_I[i], LEM_Down_I[i], Hall_T, title, tmin, tmax)
 
 ###################### Heinzinger
-if not os.path.isdir(sys.argv[4] + "/Cathode"):
-  os.system("mkdir " + sys.argv[4] + "/Cathode")
-
-a = plot_V_I(time, date, Cathode_V, Cathode_I, "cathode", "./" + sys.argv[4] + "/Cathode/", tmin, tmax)
-###################### Grid
-if not os.path.isdir(sys.argv[4] + "/Grid"):
-  os.system("mkdir " + sys.argv[4] + "/Grid")
+if Cathode_V[0] != "ignore" and Cathode_I[0] != "ignore":
+  if not os.path.isdir(sys.argv[4] + "/Cathode"):
+    os.system("mkdir " + sys.argv[4] + "/Cathode")
+  a = plot_V_I(time, date, Cathode_V, Cathode_I, "cathode", "./" + sys.argv[4] + "/Cathode/", tmin, tmax)
   
-a = plot_V_I(time, date, GRID_1_V, GRID_1_I, "Grid", "./" + sys.argv[4] + "/Grid/", tmin, tmax)
+###################### Grid
+if GRID_1_V[0] != "ignore" and GRID_1_I[0] != "ignore":
+  if not os.path.isdir(sys.argv[4] + "/Grid"):
+    os.system("mkdir " + sys.argv[4] + "/Grid")
+  a = plot_V_I(time, date, GRID_1_V, GRID_1_I, "Grid", "./" + sys.argv[4] + "/Grid/", tmin, tmax)
 
 ###################### all V and I
-if not os.path.isdir(sys.argv[4] + "/all_V_I"):
-  os.system("mkdir " + sys.argv[4] + "/all_V_I")
-  
-a = plot_all_V_I(time, date, Cathode_V, Cathode_I, GRID_1_V, GRID_1_I, LEM_Up_V, LEM_Up_I, LEM_Down_V, LEM_Down_I, levelmeters[6], tmin, tmax)
-  
-a = plot_all_LEMs_V_I(time, date, LEM_Up_V, LEM_Up_I, LEM_Down_V, LEM_Down_I, GRID_1_V, tmin, tmax)
+plot_LEMS = True
+for i in range(0,len(LEM_Down_V)):
+  if LEM_Down_V[i][0] == "ignore" or LEM_Down_I[i][0] == "ignore" or LEM_Up_V[i][0] == "ignore" or LEM_Up_I[i][0] == "ignore":
+    plot_LEMs = False
+    
+plot_LV = True
+for i in range(0,len(levelmeters)):
+  if levelmeters[i][0] == "ignore":
+    plot_LV = False
+    
+if GRID_1_V[0] != "ignore" and GRID_1_I[0] != "ignore" and plot_LEMs == True:
+  if not os.path.isdir(sys.argv[4] + "/all_V_I"):
+    os.system("mkdir " + sys.argv[4] + "/all_V_I")
+  a = plot_all_LEMs_V_I(time, date, LEM_Up_V, LEM_Up_I, LEM_Down_V, LEM_Down_I, GRID_1_V, tmin, tmax)
+  if Cathode_V[0] != "ignore" and Cathode_I[0] != "ignore" and plot_LV == True:
+    a = plot_all_V_I(time, date, Cathode_V, Cathode_I, GRID_1_V, GRID_1_I, LEM_Up_V, LEM_Up_I, LEM_Down_V, LEM_Down_I, levelmeters[6], tmin, tmax)
 
 
 ###################### LevelMeters
-if not os.path.isdir(sys.argv[4] + "/LevelMeters"):
-  os.system("mkdir " + sys.argv[4] + "/LevelMeters")
-  
-a = plot_LM(time, date, levelmeters, "levelmeters", tmin, tmax)
-a = Plot_LM_RMS(time, date, levelmeters, "levelmeters_RMS", tmin, tmax)
-a = plot_LM_Delta(time, date, levelmeters, 8, "levelmeters_delta_8", tmin, tmax)
+if plot_LV == True:
+  if not os.path.isdir(sys.argv[4] + "/LevelMeters"):
+    os.system("mkdir " + sys.argv[4] + "/LevelMeters")
+  a = plot_LM(time, date, levelmeters, "levelmeters", tmin, tmax)
+  a = Plot_LM_RMS(time, date, levelmeters, "levelmeters_RMS", tmin, tmax)
+  a = plot_LM_Delta(time, date, levelmeters, 8, "levelmeters_delta_8", tmin, tmax)
 
 ###################### Box
 #if not os.path.isdir(sys.argv[4] + "/Box"):
